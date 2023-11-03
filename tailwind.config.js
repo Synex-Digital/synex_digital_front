faa7cad59de1da35dfe893b1548c406019c6ea30
esmail <120841700+hossainmdismail@users.noreply.github.com>
2023-11-03 11:21:14 -0700

    change layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        'lg': '1320px', // You can adjust the value to your desired maximum width
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
